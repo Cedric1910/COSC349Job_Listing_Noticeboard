a0582f2f93020daca8f02557ab9a37e0fdef95c2
Cedric Stephani <cstephani@oucs1601lnx.student.uod.otago.ac.nz>
2020-08-05 11:32:47 +1200

    created a setup sql file

---
+++ setup-database.sql
@@ -0,0 +1,11 @@
+CREATE TABLE JOB_LISTING
+( full_name varchar(50) not null,
+  location varchar(30) not null,
+  job_title varchar(30) not null,
+  descripition varchar(500) not null,
+  date_posted Date
+  );
+
+INSERT INTO JOB_LISTING VALUES('Cedric Stephani', 'Dunedin', 'Junior front-end developer', 'description of the job', TO_DATE('20/07/2020','DD/MM/YYYY');
+INSERT INTO JOB_LISTING VALUES('Hugo Baird', 'Dunedin', 'Junior back-end developer', 'description of the job', TO_DATE('15/04/2020','DD/MM/YYYY'); 
+
