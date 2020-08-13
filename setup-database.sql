2fc1ac17e0c0c87e7a8303b79f39330328d979aa
Hugo Baird <hugonzb@gmail.com>
2020-08-13 17:46:23 +1200

    Added more dummy data

--- setup-database.sql
+++ setup-database.sql
@@ -6,6 +6,9 @@ CREATE TABLE JOB_LISTING
   date_posted Date
   );
 
-INSERT INTO JOB_LISTING VALUES('Cedric Stephani', 'Dunedin', 'Junior front-end developer', 'description of the job', TO_DATE('20/07/2020','DD/MM/YYYY');
-INSERT INTO JOB_LISTING VALUES('Hugo Baird', 'Dunedin', 'Junior back-end developer', 'description of the job', TO_DATE('15/04/2020','DD/MM/YYYY'); 
+INSERT INTO JOB_LISTING VALUES('Cedric Stephani', 'Dunedin', 'Junior front-end developer', 'I need a junior level front-end developer to join my team', TO_DATE('20/07/2020','DD/MM/YYYY');
+INSERT INTO JOB_LISTING VALUES('Hugo Baird', 'Dunedin', 'Junior back-end developer', 'Looking for a proficient junior back-end developer to help with my project', TO_DATE('15/05/2020','DD/MM/YYYY'); 
+INSERT INTO JOB_LISTING VALUES('Evan Rashad', 'Christchurch', 'Laborer', 'I need someone to help move furniture for me', TO_DATE('15/08/2020','DD/MM/YYYY'); 
+INSERT INTO JOB_LISTING VALUES('Kevin Smith', 'Auckland', 'Gardener', 'Looking for anyone who can do some weeding for my property', TO_DATE('13/08/2020','DD/MM/YYYY'); 
+INSERT INTO JOB_LISTING VALUES('Richard Anderson', 'Wellington', 'Math Tutor', 'Currently need a math tutor to teach my 12-year old son algebra', TO_DATE('27/07/2020','DD/MM/YYYY'); 
 
