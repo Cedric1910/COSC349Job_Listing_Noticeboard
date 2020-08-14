81eb8c3dfb3c6bc1f3b9efbec37f828262a57418
Hugo Baird <hbaird@oucs1601mac.student.uod.otago.ac.nz>
2020-08-14 13:48:59 +1200

    Fixed table syntax

--- setup-database.sql
+++ setup-database.sql
@@ -1,9 +1,9 @@
 CREATE TABLE JOB_LISTING
-( full_name varchar(50) not null,
-  location varchar(30) not null,
-  job_title varchar(30) not null,
-  descripition varchar(500) not null,
-  date_posted varchar(20) not null
+( full_name varchar(50),
+  location varchar(30),
+  job_title varchar(30),
+  description varchar(500),
+  date_posted varchar(20) 
   );
 
 INSERT INTO JOB_LISTING VALUES('Cedric Stephani', 'Dunedin', 'Junior front-end developer', 'I need a junior level front-end developer to join my team', '12/03/2020');
