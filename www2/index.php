84fc6719e61238018745765efdcd07bdb7a9094c
Hugo Baird <hbaird@oucs1603mac.student.uod.otago.ac.nz>
2020-08-17 17:00:08 +1200

    Job listing noticeboard php configuration

--- www2/index.php
+++ www2/index.php
@@ -2,13 +2,13 @@
 
 <html>
   <head>
-    <title> Listings page </title>
+    <title> Job Listings Noticeboard  </title>
     <link rel="stylesheet" type="text/css" href="style.css">
   </head>
 
   <body>
     <h1> Job Listings:</h1>
-    <p> This shows all the listings currently on offer:</p>
+    <p> This shows all the jobs currently available</p>
     <table border="1">
       <tr>
         <th> name: </th>
@@ -17,21 +17,18 @@
         <th> job title </th>
         <th> description </th>
       </tr>
-
         <?php
            $db_host = '192.168.2.2';
-           $db_name = 'joblisting20'
            $db_user = 'dbuser';
            $db_passwd = 'joblisting20'
+           $db_name = 'joblistingdb';
            $pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
-           $q = $pdo->query("SELECT * FROM papers");
+           $q = $pdo->query("SELECT * FROM JOB_LISTING");
 
           while($row = $q->fetch()){
           echo "<tr><td>".$row["full_name"]."<tr><td>".$row["location"]."<tr><td>".$row["date_posted"]."<tr><td>".$row["job_title"]."<tr><td>".$row["description"]."</td></tr>\n"; 
           }
         ?> 
       </table>
-      
   </body> 
-
 </html> 
